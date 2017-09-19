1ab154024555ad0385dd35db34965777d1822ff4
Ethan Chau <echau18@cs.uw.edu>
2017-09-18 22:54:31 -0700

    real init

---
+++ brunch-config.js
@@ -0,0 +1,11 @@
+// See http://brunch.io for documentation.
+exports.files = {
+    javascripts: {joinTo: 'app.js'},
+    stylesheets: {joinTo: 'app.css'}
+};
+
+module.exports.plugins = {
+    babel: {
+        presets: ['env', 'react'],
+    }
+}
